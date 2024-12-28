a21c9cc0449b3a7ed74fac13e2245c80acce80ac
Raunak Kumar <raunakjsr7016@gmail.com>
2024-12-28 23:48:30 +0530

    second commit

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -7,6 +7,7 @@ import mongoose from "mongoose";
 import { connectToSocket } from "./controllers/socketManager.js";
 
 import cors from "cors";
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 const server = createServer(app);
@@ -16,6 +17,8 @@ app.set("port", (process.env.PORT || 8000))
 app.use(cors());
 app.use(express.json({limit: "40kb"}));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
+
+app.use("/api/v1/users", userRoutes);
 
 app.get("/home" , (req,res)=> {
     return res.json({"hello": "World"})
